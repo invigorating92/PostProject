485c395a72f3c3ec625007cd4c58e936eeef5a8f
invigorating92 <84106746+invigorating92@users.noreply.github.com>
2024-03-13 01:41:24 +0900

    Feat : Post Update 추가

--- src/main/java/org/example/post/service/PostService.java
+++ src/main/java/org/example/post/service/PostService.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface PostService {
     Long addPost(PostDto.PostCreateDto postDto);
 
+    void updatePost(Long postId, PostDto.PostUpdateDto postDto);
+
     PostDto.PostResponseDto findPost(Long postId);
 
     List<PostDto.PostResponseDto> findAll();
